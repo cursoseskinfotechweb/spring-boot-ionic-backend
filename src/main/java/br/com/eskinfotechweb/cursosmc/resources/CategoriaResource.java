1d050ad084aa4983826e6a9b05eb9224aa59d06f
Edson Shideki Kokado <eskokado@gmail.com>
2018-03-16 18:43:56 -0300

    Criando repository e service para Categoria

--- src/main/java/br/com/eskinfotechweb/cursosmc/resources/CategoriaResource.java
+++ src/main/java/br/com/eskinfotechweb/cursosmc/resources/CategoriaResource.java
@@ -1,30 +1,28 @@
 package br.com.eskinfotechweb.cursosmc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.eskinfotechweb.cursosmc.domain.Categoria;
+import br.com.eskinfotechweb.cursosmc.services.CategoriaService;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@RequestMapping(method=RequestMethod.GET)
-	public List<Categoria> listar() {
+	@Autowired
+	private CategoriaService service;
 		
-		Categoria cat1 = new Categoria(1, "Informática");
-		Categoria cat2 = new Categoria(2, "Escritório");
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
 		
-		List<Categoria> lista = new ArrayList<>();
+		Categoria obj = service.buscar(id);
 		
-		lista.add(cat1);
-		lista.add(cat2);
-		
-		return lista;
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
---
+++ src/main/java/br/com/eskinfotechweb/cursosmc/services/CategoriaService.java
@@ -0,0 +1,19 @@
+package br.com.eskinfotechweb.cursosmc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.eskinfotechweb.cursosmc.domain.Categoria;
+import br.com.eskinfotechweb.cursosmc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Categoria obj = repo.findOne(id);
+		return obj;
+	}
+}
