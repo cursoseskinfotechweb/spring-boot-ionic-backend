247d80015c366c98db58f0d3b443fc497ff8f7dc
Edson Shideki Kokado <eskokado@gmail.com>
2018-03-16 19:19:50 -0300

    Produto e associacao muitos para muitos

--- src/main/java/br/com/eskinfotechweb/cursosmc/CursomcApplication.java
+++ src/main/java/br/com/eskinfotechweb/cursosmc/CursomcApplication.java
@@ -8,13 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import br.com.eskinfotechweb.cursosmc.domain.Categoria;
+import br.com.eskinfotechweb.cursosmc.domain.Produto;
 import br.com.eskinfotechweb.cursosmc.repositories.CategoriaRepository;
+import br.com.eskinfotechweb.cursosmc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	
+	@Autowired
+	private ProdutoRepository produtoRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -26,5 +31,19 @@ public class CursomcApplication implements CommandLineRunner{
 		Categoria cat2 = new Categoria(null, "Escritório");
 		
 		categoriaRepository.save(Arrays.asList(cat1, cat2));
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
+		
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		
+		categoriaRepository.save(Arrays.asList(cat1, cat2));
+		produtoRepository.save(Arrays.asList(p1, p2, p3));
 	}
 }
