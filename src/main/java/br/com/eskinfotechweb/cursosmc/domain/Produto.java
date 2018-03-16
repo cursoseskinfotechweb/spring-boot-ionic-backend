1d0fdff10faa91e7c3eadc9aacb9066d13854561
Edson Shideki Kokado <eskokado@gmail.com>
2018-03-16 19:39:23 -0300

    Ajuste no endpoind /categorias/{id}

--- src/main/java/br/com/eskinfotechweb/cursosmc/domain/Produto.java
+++ src/main/java/br/com/eskinfotechweb/cursosmc/domain/Produto.java
@@ -9,9 +9,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinColumns;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 @Entity
 public class Produto implements Serializable {
@@ -22,7 +23,8 @@ public class Produto implements Serializable {
 	private Integer id;
 	private String nome;
 	private Double preco;
-
+	
+	@JsonBackReference
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
--- src/main/java/br/com/eskinfotechweb/cursosmc/services/CategoriaService.java
+++ src/main/java/br/com/eskinfotechweb/cursosmc/services/CategoriaService.java
@@ -5,15 +5,20 @@ import org.springframework.stereotype.Service;
 
 import br.com.eskinfotechweb.cursosmc.domain.Categoria;
 import br.com.eskinfotechweb.cursosmc.repositories.CategoriaRepository;
+import br.com.eskinfotechweb.cursosmc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
 		Categoria obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName());
+		}
 		return obj;
 	}
 }
