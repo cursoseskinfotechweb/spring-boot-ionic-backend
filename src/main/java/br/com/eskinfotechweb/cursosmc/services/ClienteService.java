5a3631c3fa40a836c2b0a705174bffe0f899f242
Edson Shideki Kokado <eskokado@gmail.com>
2018-03-17 16:35:37 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/br/com/eskinfotechweb/cursosmc/services/ClienteService.java
@@ -0,0 +1,25 @@
+package br.com.eskinfotechweb.cursosmc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.eskinfotechweb.cursosmc.domain.Cliente;
+import br.com.eskinfotechweb.cursosmc.repositories.ClienteRepository;
+import br.com.eskinfotechweb.cursosmc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		if (obj == null) {
+			throw new ObjectNotFoundException(
+					"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+	}
+
+}
