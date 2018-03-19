7a086a1ffe0c9b3901fd2c6acc0791032fa65ee4
Edson Shideki Kokado <eskokado@gmail.com>
2018-03-19 12:35:30 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/br/com/eskinfotechweb/cursosmc/services/ClienteService.java
+++ src/main/java/br/com/eskinfotechweb/cursosmc/services/ClienteService.java
@@ -1,10 +1,18 @@
 package br.com.eskinfotechweb.cursosmc.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.eskinfotechweb.cursosmc.domain.Cliente;
+import br.com.eskinfotechweb.cursosmc.dto.ClienteDTO;
 import br.com.eskinfotechweb.cursosmc.repositories.ClienteRepository;
+import br.com.eskinfotechweb.cursosmc.services.exceptions.DataIntegrityException;
 import br.com.eskinfotechweb.cursosmc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -22,4 +30,36 @@ public class ClienteService {
 		return obj;
 	}
 
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
+
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.delete(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir porque há entidades relacionadas!");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
 }
